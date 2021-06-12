336b37c560b4b7e46bedaf3e9dfe1139a8aa6bd6
Fitri Anggraini <fitrianggraini96@gmail.com>
2021-06-12 12:19:40 +0700

    create data migration & model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->tinyInteger('user_type')->nullable()->comment('1 => Admin, 2 => Doctor, 3 => Patient');
+            $table->date('date_of_birth')->nullable();
+            $table->string('mobile_number')->unique();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
---
+++ database/migrations/2021_06_12_045342_create_schedules_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('patient_id');
+            $table->unsignedBigInteger('doctor_id');
+            $table->date('date')->nullable();
+            $table->time('time')->nullable();
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            $table->foreign('patient_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
