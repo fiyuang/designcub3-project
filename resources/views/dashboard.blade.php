ba740f96f1cc2d6d6f3a2f990c4cf20298128e8f
Fitri Anggraini <fitrianggraini96@gmail.com>
2021-06-13 10:59:24 +0700

    cleaning code dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,307 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
+@section('content')
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Klinik Cub3 - Dashboard Admin</title>
-
-    <!-- Custom fonts for this template-->
-    <link href="{{ asset('vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
-
-    <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
-    <link href="vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
-    <!-- Page Wrapper -->
-    <div id="wrapper">
-
-        <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-            <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-                <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
-                </div>
-                <div class="sidebar-brand-text mx-3">Klinik Cub<sup>3</sup></div>
-            </a>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider my-0">
-
-            <!-- Nav Item - Dashboard -->
-            <li class="nav-item active">
-                <a class="nav-link" href="index.html">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider">
-
-            <!-- Heading -->
-            <div class="sidebar-heading">
-                Interface
-            </div>
-
-            <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
-            </li>
-
-            <!-- Divider -->
-            <hr class="sidebar-divider d-none d-md-block">
-
-            <!-- Sidebar Toggler (Sidebar) -->
-            <div class="text-center d-none d-md-inline">
-                <button class="rounded-circle border-0" id="sidebarToggle"></button>
-            </div>
-
-        </ul>
-        <!-- End of Sidebar -->
-
-        <!-- Content Wrapper -->
-        <div id="content-wrapper" class="d-flex flex-column">
-
-            <!-- Main Content -->
-            <div id="content">
-
-                <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-
-                        <div class="topbar-divider d-none d-sm-block"></div>
-
-                        <!-- Nav Item - User Information -->
-                        <li class="nav-item dropdown no-arrow">
-                            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin</span>
-                                <img class="img-profile rounded-circle"
-                                    src="img/undraw_profile.svg">
-                            </a>
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
-
-                    </ul>
-
-                </nav>
-                <!-- End of Topbar -->
-
-                <!-- Begin Page Content -->
-                <div class="container-fluid">
-
-                    <!-- Page Heading -->
-                    <div class="d-sm-flex align-items-center justify-content-between mb-4">
-                        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                    </div>
-
-                    <!-- DataTales Example -->
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Jadwal Periksa Gigi</h6>
-                        </div>
-                        <div class="card-body">
-                            <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                                    <thead>
-                                        <tr>
-                                            <th>No.</th>
-                                            <th>Nama Pasien</th>
-                                            <th>Dokter</th>
-                                            <th>Tanggal Periksa</th>
-                                            <th>Waktu Periksa</th>
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @php $i = 1 @endphp
-                                        @foreach ($schedules as $schedule)
-                                        <tr>
-                                            <td>{{ $i++ }}</td>
-                                            <td>{{ $schedule->patient->name }}</td>
-                                            <td>{{ $schedule->doctor->name }}</td>
-                                            <td>{{ $schedule->date }}</td>
-                                            <td>{{ $schedule->time }}</td>
-                                            <td><a href="javascript:void(0)" data-toggle="tooltip" data-id="{{ $schedule->id }}" class="btn btn-primary btn-sm detail-patient ml-1"><i class="fas fa-eye fa-fw"></i> Detail</a></td>
-                                        </tr>
-                                        @endforeach
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-
-                </div>
-                <!-- /.container-fluid -->
-
-            </div>
-            <!-- End of Main Content -->
-
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Klinik Gigi Cub3</span>
-                    </div>
-                </div>
-            </footer>
-            <!-- End of Footer -->
-
-        </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="{{ route('logout') }}">Logout</a>
-                </div>
-            </div>
-        </div>
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
     </div>
 
-    <div class="modal fade" id="detail-patient" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Detail Pasien</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <table class="table table-striped text-md">
-                    <tr>
-                        <th width="200">Nama Pasien </th>
-                        <td class="name"></td>
-                    </tr>
-                    <tr>
-                        <th width="200">No Telepon</th>
-                        <td class="mobile_number"></td>
-                    </tr>
-                    <tr>
-                        <th width="200">Tanggal Lahir </th>
-                        <td class="dob"></td>
-                    </tr>
-                    <tr>
-                        <th width="200">Nama Dokter </th>
-                        <td class="doctor"></td>
-                    </tr>
-                    <tr>
-                        <th width="200">Tanggal & Waktu Periksa </th>
-                        <td class="datetime_appointment"></td>
-                    </tr>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Jadwal Periksa Gigi</h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>No.</th>
+                            <th>Nama Pasien</th>
+                            <th>Dokter</th>
+                            <th>Tanggal Periksa</th>
+                            <th>Waktu Periksa</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php $i = 1 @endphp
+                        @foreach ($schedules as $schedule)
+                        <tr>
+                            <td>{{ $i++ }}</td>
+                            <td>{{ $schedule->patient->name }}</td>
+                            <td>{{ $schedule->doctor->name }}</td>
+                            <td>{{ $schedule->date }}</td>
+                            <td>{{ $schedule->time }}</td>
+                            <td><a href="javascript:void(0)" data-toggle="tooltip" data-id="{{ $schedule->id }}"
+                                    class="btn btn-primary btn-sm detail-patient ml-1"><i class="fas fa-eye fa-fw"></i>
+                                    Detail</a></td>
+                        </tr>
+                        @endforeach
+                    </tbody>
                 </table>
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            </div>
-            </div>
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script type="text/javascript" src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</div>
 
-    <!-- Core plugin JavaScript-->
-    <script type="text/javascript" src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+@include('components/_detail_patient')
 
-    <!-- Custom scripts for all pages-->
-    <script type="text/javascript" src="{{ asset('js/sb-admin-2.min.js') }}"></script>
+<!-- /.container-fluid -->
+@endsection
 
-    <!-- Page level plugins -->
-    <script type="text/javascript" src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+@section('scripts')
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('body').on('click', '.detail-patient', function () {
+            var id = $(this).data('id');
+            $.get('patient/' + id + '/json', function (data) {
+                console.log(data)
 
-    <!-- Page level custom scripts -->
-    <script type="text/javascript" src="{{ asset('js/demo/chart-area-demo.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/demo/chart-pie-demo.js') }}"></script>
+                var datetime_appointment = (data.date) + ", " + (data.time)
 
-    <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+                $('.name').html(data.patient.name);
+                $('.mobile_number').html(data.patient.mobile_number);
+                $('.dob').html(data.patient.date_of_birth);
+                $('.doctor').html(data.doctor.name);
+                $('.datetime_appointment').html(datetime_appointment);
 
-    <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
-
-    <script type="text/javascript">
-        $(document).ready(function() {
-            $('body').on('click', '.detail-patient', function () {
-                var id = $(this).data('id');
-                $.get('patient/' + id +'/json', function (data) {
-                    console.log(data)
-
-                    var datetime_appointment = (data.date) + ", "+ (data.time)
-
-                    $('.name').html(data.patient.name);
-                    $('.mobile_number').html(data.patient.mobile_number);
-                    $('.dob').html(data.patient.date_of_birth);
-                    $('.doctor').html(data.doctor.name);
-                    $('.datetime_appointment').html(datetime_appointment);
-                    
-                    $('#detail-patient').modal('show');
-                });
+                $('#detail-patient').modal('show');
             });
-        })
-    </script>
+        });
+    })
 
-</body>
-
-</html>
+</script>
+@endsection
